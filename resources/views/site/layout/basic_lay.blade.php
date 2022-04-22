63798d012382430e77f86436ea38e7b5d5948dd6
joao951951 <joao159@edu.unifil.br>
2022-04-22 11:53:29 -0300

    v1.1

---
+++ resources/views/site/layout/basic_lay.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>App Gestão - @yield('tittle') </title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
